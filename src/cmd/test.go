63b0de244b5dca16e4532c2afe5e2b79cbad8816
Kyle Rockman <rocktavious@icloud.com>
2022-03-22 21:42:03 -0500

    add test command that reads job from a file for localdev

---
+++ src/cmd/test.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/opslevel/opslevel-runner/pkg"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var jobFile string
+
+// previewCmd represents the preview command
+var testCmd = &cobra.Command{
+	Use:  "test",
+	RunE: doTest,
+}
+
+func init() {
+	rootCmd.AddCommand(testCmd)
+
+	testCmd.PersistentFlags().StringVarP(&jobFile, "file", "f", ".", "File to read data from. If '-' then reads from stdin. Defaults to read from './job.yaml'")
+	viper.BindPFlags(testCmd.Flags())
+}
+
+func doTest(cmd *cobra.Command, args []string) error {
+	job, err := readJobInput()
+	if err != nil {
+		return err
+	}
+
+	runner, err := pkg.NewJobRunner()
+	cobra.CheckErr(err)
+
+	return runner.Run(job)
+}
+
+func readJobInput() (*pkg.JobSchema, error) {
+	if jobFile == "" {
+		return nil, fmt.Errorf("please specify a job file")
+	}
+	if jobFile == "-" {
+		viper.SetConfigType("yaml")
+		viper.ReadConfig(os.Stdin)
+	} else if jobFile == "." {
+		viper.SetConfigFile("./job.yaml")
+	} else {
+		viper.SetConfigFile(jobFile)
+	}
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, err
+		}
+	}
+	job := &pkg.JobSchema{}
+	viper.Unmarshal(&job)
+	return job, nil
+}
