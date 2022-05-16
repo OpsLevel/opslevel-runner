2c8ee5a1c1d466b99cfd147aaf6c6f40b496b40b
Kyle Rockman <rocktavious@icloud.com>
2022-05-16 07:19:53 -0500

    go fmt file

--- src/pkg/metrics.go
+++ src/pkg/metrics.go
@@ -13,8 +13,8 @@ import (
 )
 
 var (
-	metricNamespace   = "opslevel_runner"
-	MetricJobsStarted  prometheus.Counter
+	metricNamespace      = "opslevel_runner"
+	MetricJobsStarted    prometheus.Counter
 	MetricJobsDuration   prometheus.Histogram
 	MetricJobsFinished   *prometheus.CounterVec
 	MetricJobsProcessing prometheus.Gauge
@@ -24,28 +24,28 @@ func initMetrics() {
 	runner := uuid.New().String()
 	log.Info().Msgf("Starting runner for id '%s'", runner)
 	MetricJobsStarted = promauto.NewCounter(prometheus.CounterOpts{
-		Namespace: metricNamespace,
-		Name: "jobs_started",
-		Help: "The count of jobs that started processing.",
+		Namespace:   metricNamespace,
+		Name:        "jobs_started",
+		Help:        "The count of jobs that started processing.",
 		ConstLabels: prometheus.Labels{"runner": runner},
 	})
 	MetricJobsDuration = promauto.NewHistogram(prometheus.HistogramOpts{
-		Namespace: metricNamespace,
-		Name: "jobs_duration",
-		Help: "The duration of jobs in seconds.",
+		Namespace:   metricNamespace,
+		Name:        "jobs_duration",
+		Help:        "The duration of jobs in seconds.",
 		ConstLabels: prometheus.Labels{"runner": runner},
 	})
 	MetricJobsFinished = promauto.NewCounterVec(prometheus.CounterOpts{
-		Namespace: metricNamespace,
-		Name: "jobs_finished",
-		Help: "The count of jobs that finished processing by outcome status.",
+		Namespace:   metricNamespace,
+		Name:        "jobs_finished",
+		Help:        "The count of jobs that finished processing by outcome status.",
 		ConstLabels: prometheus.Labels{"runner": runner},
 	},
-	[]string{"outcome"})
+		[]string{"outcome"})
 	MetricJobsProcessing = promauto.NewGauge(prometheus.GaugeOpts{
-		Namespace: metricNamespace,
-		Name:      "jobs_processing",
-		Help:      "The current number of active jobs being processed.",
+		Namespace:   metricNamespace,
+		Name:        "jobs_processing",
+		Help:        "The current number of active jobs being processed.",
 		ConstLabels: prometheus.Labels{"runner": runner},
 	})
 }
