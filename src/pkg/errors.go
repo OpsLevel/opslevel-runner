792228a7197a9d8fc0123db96fa41db7ce5d375a
Edgar Ochoa <edgar@opslevel.com>
2022-10-05 20:36:06 -0500

    Log error message if sentry isn't configured

--- src/pkg/errors.go
+++ src/pkg/errors.go
@@ -2,12 +2,18 @@ package pkg
 
 import (
 	"github.com/getsentry/sentry-go"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 func CheckErr(err error) {
 	if err != nil {
-		sentry.CaptureException(err)
+		if _, present := os.LookupEnv("SENTRY_DSN"); present {
+			sentry.CaptureException(err)
+		} else {
+			log.Error().Err(err).Msg("")
+		}
 	}
 	cobra.CheckErr(err)
 }
