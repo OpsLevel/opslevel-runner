90beabd7a7631d744fbb870ef1b6ef6fa986fd5a
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-23 08:05:19 -0500

    change k8s versions to 1.26.11

--- src/pkg/k8s.go
+++ src/pkg/k8s.go
@@ -370,9 +370,8 @@ func (s *JobRunner) CreatePod(config *corev1.Pod) (*corev1.Pod, error) {
 
 func (s *JobRunner) WaitForPod(podConfig *corev1.Pod, timeout time.Duration) error {
 	s.logger.Debug().Msgf("Waiting for pod %s/%s to be ready in %s ...", podConfig.Namespace, podConfig.Name, timeout)
-	immediate := true // TODO: find out what value should go here
-	return wait.PollUntilContextTimeout(context.TODO(), time.Second, timeout, immediate, func(ctx context.Context) (bool, error) {
-		pod, err := s.clientset.CoreV1().Pods(podConfig.Namespace).Get(context.TODO(), podConfig.Name, metav1.GetOptions{})
+	return wait.PollUntilContextTimeout(context.TODO(), time.Second, timeout, true, func(ctx context.Context) (bool, error) {
+		pod, err := s.clientset.CoreV1().Pods(podConfig.Namespace).Get(ctx, podConfig.Name, metav1.GetOptions{})
 		if err != nil {
 			return false, err
 		}
