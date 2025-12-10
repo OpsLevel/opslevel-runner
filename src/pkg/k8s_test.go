521aeb6853ecb5b973ba2cc5d4857783a0669cd8
Derek Etherton <derek.etherton@opslevel.com>
2025-12-10 07:57:31 -0800

    add basic securityContext unit test

--- src/pkg/k8s_test.go
+++ src/pkg/k8s_test.go
@@ -3,7 +3,10 @@ package pkg
 import (
 	"testing"
 
+	"github.com/opslevel/opslevel-go/v2024"
 	"github.com/rocktavious/autopilot/v2023"
+	"github.com/rs/zerolog"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func TestCreateLabelSelector(t *testing.T) {
@@ -17,4 +20,52 @@ func TestCreateLabelSelector(t *testing.T) {
 	// Assert
 	autopilot.Ok(t, err)
 	autopilot.Equals(t, labels, labelSelector.MatchLabels)
+}
+
+func TestGetPodObject_CodingAgentPrivileged(t *testing.T) {
+	// Arrange
+	runner := &JobRunner{
+		logger: zerolog.Nop(),
+		podConfig: &K8SPodConfig{
+			Namespace:                     "test",
+			SecurityContext:               corev1.PodSecurityContext{},
+			TerminationGracePeriodSeconds: 30,
+		},
+	}
+	job := opslevel.RunnerJob{
+		Image: "jobs-coding-agent:latest",
+	}
+	labels := map[string]string{"app": "test"}
+
+	// Act
+	pod := runner.getPodObject("test-pod", labels, job)
+
+	// Assert
+	autopilot.Assert(t, pod.Spec.Containers[0].SecurityContext != nil, "SecurityContext should be set for coding agent")
+	autopilot.Assert(t, pod.Spec.Containers[0].SecurityContext.Privileged != nil, "Privileged should be set for coding agent")
+	autopilot.Equals(t, true, *pod.Spec.Containers[0].SecurityContext.Privileged)
+	autopilot.Equals(t, int64(0), *pod.Spec.SecurityContext.RunAsUser)
+	autopilot.Equals(t, int64(0), *pod.Spec.SecurityContext.FSGroup)
+}
+
+func TestGetPodObject_RegularJobNotPrivileged(t *testing.T) {
+	// Arrange
+	runner := &JobRunner{
+		logger: zerolog.Nop(),
+		podConfig: &K8SPodConfig{
+			Namespace:                     "test",
+			SecurityContext:               corev1.PodSecurityContext{},
+			TerminationGracePeriodSeconds: 30,
+		},
+	}
+	job := opslevel.RunnerJob{
+		Image: "alpine:latest",
+	}
+	labels := map[string]string{"app": "test"}
+
+	// Act
+	pod := runner.getPodObject("test-pod", labels, job)
+
+	// Assert
+	autopilot.Equals(t, (*corev1.SecurityContext)(nil), pod.Spec.Containers[0].SecurityContext)
 }
